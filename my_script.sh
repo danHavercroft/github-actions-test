355dbcc7343257075f84ff515c325c72970ebeeb
danHavercroft <32074604+danHavercroft@users.noreply.github.com>
2024-07-25 10:21:50 +0100

    Create my_script.sh
    Create script

---
+++ my_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running automation..." > ./output_log.txt
+
+echo "" >> ./output_log.txt
+echo "Checking uptime..." >> ./output_log.txt
+echo $(uptime) >> ./output_log.txt
+
+echo "THIS IS MY UPDATED SCRIPT" >> ./output_log.txt
+echo "THIS IS MY NEW CODE" >> ./output_log.txt
